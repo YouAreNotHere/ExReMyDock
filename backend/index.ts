c6c68d46b1b6136c65f2bc365cec2682b88ad7b7
ngrinevich <ngrinevich@it-one.ru>
2024-08-10 19:08:28 +0500

    Вынес бд в отдельный класс

--- backend/index.ts
+++ backend/index.ts
@@ -1,39 +1,24 @@
 import express from 'express';
-import mysql from 'mysql2';
 import { buildRouting } from "./src/routes";
 import bodyParser from "body-parser";
 import cors from 'cors';
+import db from "./src/database/db";
 
 const app = express();
 
 const PORT = 8081;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'test2007',
-});
-
-connection.connect(function(err){
-  if (err) {
-    return console.error("Ошибка: " + err.message);
-  }
-  else{
-    console.log("Подключение к серверу MySQL успешно установлено");
-  }
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
-buildRouting(app);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+app.listen(PORT, async () => {
+  console.log(`App listening on port ${PORT}`)
+
+  await db.createTables();
+  buildRouting(app);
 })
---
+++ backend/src/database/db.ts
@@ -0,0 +1,65 @@
+import {createPool, Pool} from 'mysql2/promise';
+
+class db {
+  private pool: Pool;
+
+  constructor() {
+    this.pool = createPool({
+      host: 'localhost',
+      user: 'root',
+      password: '1234',
+      database: 'test2007',
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
+    });
+    console.log('MySQL pool was created...');
+  }
+
+  public async query(sql: string) {
+    try {
+      const connection = await this.pool.getConnection();
+      console.log('MySQL connected...')
+
+      const [rows, fields] = await connection.query(sql);
+      connection.release();
+      console.log('MySQL released...')
+
+      return { rows, fields };
+    } catch (error) {
+      console.error(`
+        !Error while mysql query. \n
+        Error: ${error}.\n
+        Query: ${sql}.\n
+      `);
+    }
+  }
+
+  public async close() {
+    try {
+      await this.pool.end();
+      console.log('MySQL connection closed.');
+    } catch (error) {
+      console.log('Error while closing the connection: ', error);
+    }
+  }
+
+  public async createTables() {
+    await this.createUsersTable();
+
+    console.log('MySQL tables was created');
+  }
+
+  private async createUsersTable() {
+    const sql = `
+      CREATE TABLE IF NOT EXISTS users (
+          id INT AUTO_INCREMENT PRIMARY KEY,
+          name VARCHAR(100) NOT NULL,
+          password VARCHAR(255) NOT NULL
+      );
+    `;
+    await this.query(sql);
+  }
+}
+
+export default new db();
