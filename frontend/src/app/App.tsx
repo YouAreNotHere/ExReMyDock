bc9806f4ea89927be128ee3637feeae907b74ef7
ngrinevich <ngrinevich@it-one.ru>
2024-08-11 17:23:34 +0500

    Добавил роутинг на фронте

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,8 +1,22 @@
 import './App.css';
-import {SignupPage} from "../pages/auth/Signup.page";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import SignupPage from "../pages/auth/Signup.page";
+import MainPage from "../pages/Main.page";
+import NotFoundPage from "../pages/not-found.page";
 
 const App = () => {
-  return <div className="App"><SignupPage /></div>;
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/signup' element={<SignupPage />} />
+          <Route path='/' element={<MainPage />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App;
--- frontend/src/pages/auth/Signup.page.tsx
+++ frontend/src/pages/auth/Signup.page.tsx
@@ -11,4 +11,4 @@ const SignupPage = () => {
   )
 };
 
-export {SignupPage};
+export default SignupPage;
