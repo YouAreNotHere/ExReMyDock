4a7b21f18f182975348c6b2faa9452ec694b30c4
ngrinevich <ngrinevich@it-one.ru>
2024-10-10 23:16:04 +0500

    Добавил хук useArrowNavigation

---
+++ frontend/src/shared/hooks/useArrowNavigation.ts
@@ -0,0 +1,58 @@
+import { RefObject, useCallback, useEffect, useState } from 'react';
+
+const useArrowNavigation = (refs: RefObject<any>[]) => {
+  const [focusableElementIndex, setFocusableElementIndex] = useState(0);
+
+  const changeFocusByKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      if (!refs.every((ref) => !!ref.current)) return;
+
+      if (event.key === 'ArrowUp') {
+        const nextIndex =
+          focusableElementIndex === 0
+            ? refs.length - 1
+            : focusableElementIndex - 1;
+
+        refs[nextIndex].current.focus();
+
+        setFocusableElementIndex(nextIndex);
+      }
+
+      if (event.key === 'ArrowDown') {
+        const nextIndex =
+          focusableElementIndex === refs.length - 1
+            ? 0
+            : focusableElementIndex + 1;
+
+        refs[nextIndex].current.focus();
+
+        setFocusableElementIndex(nextIndex);
+      }
+    },
+    [focusableElementIndex, refs],
+  );
+
+  const changeFocusByClick = (event: MouseEvent) => {
+    if (!event.target) return;
+
+    const clickedIndex = refs.findIndex(
+      (ref) => ref.current.id === (event.target as any).id,
+    );
+
+    if (clickedIndex !== -1) {
+      setFocusableElementIndex(clickedIndex);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', changeFocusByKeyDown);
+    window.addEventListener('click', changeFocusByClick);
+
+    return () => {
+      window.removeEventListener('keydown', changeFocusByKeyDown);
+      window.removeEventListener('click', changeFocusByClick);
+    };
+  }, [refs]);
+};
+
+export { useArrowNavigation };
