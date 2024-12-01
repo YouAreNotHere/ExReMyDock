4c72510c6ed18bfabc12a2da3a57364543e27d94
YouAreNotHere <malferety@gmail.com>
2024-12-01 14:08:07 +0500

    Test2

--- backend/index.ts
+++ backend/index.ts
@@ -8,22 +8,27 @@ import { sessionMiddleware } from './src/middlewares/session.middleware';
 import path from "node:path";
 
 const app = express();
-let PORT: any;
-if (process.env.NODE_ENV === "production") {
-  PORT = process.env.PORT || 3000;
+// let PORT: any;
+// if (process.env.NODE_ENV === "production") {
+//   PORT = process.env.PORT || 3000;
 
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+//   app.use(express.static(path.join(__dirname, '../frontend/build')));
+//   app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+//   });
+// }else{
+//   PORT = 3001;
+//   console.log("⚠️ Not seeing your changes as you develop?");
+//   console.log(
+//       "⚠️ Do you need to set 'start': 'npm run development' in package.json?"
+//   );
+// }
+
+const PORT = 8081;
+app.use(express.static(path.join(__dirname, '../frontend/build')));
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
   });
-}else{
-  PORT = 3001;
-  console.log("⚠️ Not seeing your changes as you develop?");
-  console.log(
-      "⚠️ Do you need to set 'start': 'npm run development' in package.json?"
-  );
-}
-
 
 initMiddlewares(app);
 initRouting(app);
