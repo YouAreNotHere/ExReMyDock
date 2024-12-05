d17f412ccb4ec45b8a0ee62f4ff92c9ed7153128
YouAreNotHere <malferety@gmail.com>
2024-12-05 20:42:35 +0500

    Установил SCSS, побаловался с :root

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,24 @@
-import './App.css';
+import './App.scss';
 import { Navigation } from './ui';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+  }, [isDarkMode]);
+
   return (
     <div className='App'>
       <Navigation />
+      <button onClick={() => setIsDarkMode(!isDarkMode)}>
+        Dark mode
+      </button>
     </div>
   );
 };
