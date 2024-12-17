928ed59233bbf44a3668529003bd9b2f7c6cd897
YouAreNotHere <malferety@gmail.com>
2024-12-17 15:05:18 +0500

    redeploy2

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -52,7 +52,7 @@ class db {
   public async createTables() {
     await this.createUsersTable();
     await this.createTodosTable();
-    // await this.changeFormat();
+    await this.changeFormat();
 
     console.log('MySQL tables was created');
   }
@@ -83,7 +83,7 @@ class db {
 
   private async changeFormat(){
     const sql = `DROP TABLE todos;`;
-    const sql1 = `ALTER DATABASE test_database CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;`;
+    const sql1 = `ALTER DATABASE sql7752582 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;`;
     const sql2 = `ALTER TABLE todos CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;`;
     const sql3 = `ALTER TABLE todos MODIFY text TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;`;
     await this.query(sql1);
